a1b10b5986e2a06d6f34b4be3720157e9b6d61ce
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-11 17:19:47 -0300

    create function that fills an array with n random letters

--- front/src/components/LetterBlocks.jsx
+++ front/src/components/LetterBlocks.jsx
@@ -2,20 +2,36 @@
 import { useLanguageContext } from "../hooks/useLanguageContext";
 import { dictionaryMapping } from "../utilities/dictionaries";
 
+function selectRandomLetters(dictionary, blockQuantity){
+
+    let randomLettersArray = []
+
+    while(randomLettersArray.length < blockQuantity){
+        const randomIndex = Math.floor(Math.random() * dictionary.length);
+        const randomLetter = dictionary[randomIndex];
+        if(!(randomLetter in randomLettersArray)){
+            randomLettersArray.push(randomLetter)
+        }
+    }
+    
+    console.log(randomLettersArray)
+
+    return randomLettersArray
+}
+
 const LetterBlocks = (props) => {
     const blockQuantity = props.blockQuantity;
     const { language } = useLanguageContext()
     const dictionary = dictionaryMapping[language]
 
-    const keys = Object.keys(dictionary);
-    const randomIndex = Math.floor(Math.random() * keys.length);
-    const randomKey = keys[randomIndex];
+    const dictionaryArray = Object.keys(dictionary);
+    const randomLetters = selectRandomLetters(dictionaryArray, blockQuantity)
 
     return ( 
     <div className="letter-block-container">
         <div className="block">
-            <div className="letter">{ randomKey }</div>
-            <div className="transliteration">{ dictionary[randomKey] }</div>
+            <div className="letter">{ randomLetters[0] }</div>
+            <div className="transliteration">{ dictionary[randomLetters[0]] }</div>
         </div>
     </div> );
 }

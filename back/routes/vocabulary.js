a0f5ded78ca24ca79afe9a9c278a26b33f5295d5
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2024-03-25 12:32:30 -0300

    set up routes

--- back/routes/vocabulary.js
+++ back/routes/vocabulary.js
@@ -1,13 +1,30 @@
-import { Router } from 'express'
+const express = require('express')
 
 const router = express.Router()
 
-//GET all entrys from given language
+//GET all entries from given language
+router.get('/:language/', (req,res) => {
+    res.json({mssg:'GET all words'})
+})
 
 //GET single entry from given language
+router.get('/:language/:id', (req, res) => {
+    res.json({mssg:'GET single words'})
+})
 
-//POST a entry from a given language
+//POST an entry for a given language
+router.post('/:language/', (req, res) => {
+    res.json({mssg:'POST a word'})
+})
 
-//DELETE a entry from a given language
+//DELETE an entry for a given language
+router.delete('/:language/:id', (req, res) => {
+    res.json({mssg:'DELETE WORD'})
+})
 
-//UPDATE a entry from a given language
+//UPDATE an entry for a given language
+router.put('/:language/:id', (req, res) => {
+    res.json({mssg:'UPDATE WORD'})
+})
+
+module.exports = router
--- back/server.js
+++ back/server.js
@@ -1,22 +1,22 @@
 require('dotenv').config()
 
 const express = require('express');
+const routes = require('./routes/vocabulary')
 
 const app = express()
 
 //middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get('/', (req, res)=>{
-    res.json({mssg:'Welcome to app'})
-})
+app.use('/api/vocabulary', routes)
 
 //listen to requests
 
 app.listen(process.env.PORT, () => {
-    console.log('Listening on port' + process.env.PORT)
+    console.log('Listening on port ' + process.env.PORT)
 })
